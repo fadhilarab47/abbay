745d5fef462591233fa830304e2660572df3b7c4
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-06-24 16:31:40 +0700

    Update listgroup.py

--- YukkiMusic/plugins/tools/listgroup.py
+++ YukkiMusic/plugins/tools/listgroup.py
@@ -2,15 +2,11 @@ from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 from pyrogram.types import ChatPrivileges, Message
 import time
-import asyncio 
-
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-
-from pyrogram import Client, filters
+import asyncio
 from pymongo import MongoClient
 from config import MONGO_DB_URI
-
+from YukkiMusic import app
+from YukkiMusic.misc import SUDOERS
 
 # Inisialisasi MongoDB client
 mongo_client = MongoClient(MONGO_DB_URI)
@@ -18,7 +14,7 @@ db = mongo_client.grouplist
 groups_collection = db.groups
 
 @app.on_message(filters.group & filters.new_chat_members)
-async def new_group_handler(client, message):
+async def new_group_handler(client: Client, message: Message):
     chat_id = message.chat.id
     chat_name = message.chat.title
 
@@ -28,15 +24,12 @@ async def new_group_handler(client, message):
         groups_collection.insert_one({"chat_id": chat_id, "chat_name": chat_name})
         await message.reply_text(f"Grup '{chat_name}' dengan ID {chat_id} telah ditambahkan ke database.")
 
-@app.on_message(filters.command("groups") & SUDOERS)
-async def list_groups_handler(client, message):
+@app.on_message(filters.command("listgroups") & SUDOERS)
+async def list_groups_handler(client: Client, message: Message):
     # Ambil daftar grup dari database
     groups = groups_collection.find()
     response = "Daftar grup yang menggunakan bot ini:\n"
     for group in groups:
         response += f"{group['chat_name']} (ID: {group['chat_id']})\n"
     await message.reply_text(response)
-
-if __name__ == "__main__":
-    bot.run()
     
