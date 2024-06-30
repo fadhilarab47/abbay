18c20bda800e05484ebf561ea9f0f8b6d99cedb7
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-06-30 15:38:03 +0700

    Update Youtube.py

--- YukkiMusic/platforms/Youtube.py
+++ YukkiMusic/platforms/Youtube.py
@@ -10,8 +10,8 @@ from yt_dlp import YoutubeDL
 from youtubesearchpython.__future__ import VideosSearch
 
 from config import *
-from YukkiMusic.utility.database import is_on_off
-from YukkiMusic.utility.formatters import time_to_seconds
+from YukkiMusic.utils.database import is_on_off
+from YukkiMusic.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
